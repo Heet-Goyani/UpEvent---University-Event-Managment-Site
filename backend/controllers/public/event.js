d765588393c24d995c080a84a8dbaa4dc2c1ab9a
Himanshu Kabra <himanshukabra2212@gmail.com>
2024-07-01 10:32:01 +0530

    public listevent api updates

--- backend/controllers/public/event.js
+++ backend/controllers/public/event.js
@@ -1,4 +1,5 @@
-// Controller imports
+import { QueryTypes } from "sequelize";
+import { sequelize } from "../../db/connection.js";// Controller imports
 
 // Middleware imports
 
@@ -21,18 +22,12 @@ const getEventList = async (req, res) => {
     if (req.body.organiser) {
       where.organiser = req.query.organiser;
     }
-    const events = await Event.findAll({
-      where,
-      raw: true,
-      include: {
-        model: Organiser,
-        as: "organiser",
-        attributes: {
-          exclude: ["password"],
-        },
-      },
-      nest: true,
-    });
+    const events = await sequelize.query(
+      `with eventorganiser as (select * from organisers) select l.id, l.name, l.description, l.genre, l.date, l.time, l.notificationDate, l.notificationTime, l.available, l.venue, l.meetLink, l.personalizedRegisteration, l.registerationLink, l.coverImage, l.reachUsAt, l.instagram, l.facebook, l.twitter, l.linkedin, r3.name as organiserCollege, r3.profilePic as organiserProfilePic, r3.coverPic as organiserCoverPic from  events as l left join eventorganiser as r3 on l.organiserId = r3.id;`,
+      {
+        type: QueryTypes.SELECT,
+      }
+    );
     if (!events) return res.status(404).json({ message: "No events found" });
     return res.status(200).json({ events: events });
   } catch (error) {
@@ -43,20 +38,14 @@ const getEventList = async (req, res) => {
 
 const getEvent = async (req, res) => {
   try {
-    const event = await Event.findOne({
-      where: { id: req.params.id },
-      raw: true,
-      include: {
-        model: Organiser,
-        as: "organiser",
-        attributes: {
-          exclude: ["password"],
-        },
-      },
-      nest: true,
-    });
+    const event = await sequelize.query(
+      `with eventorganiser as (select * from organisers) select l.id, l.name, l.description, l.genre, l.date, l.time, l.notificationDate, l.notificationTime, l.available, l.venue, l.meetLink, l.personalizedRegisteration, l.registerationLink, l.coverImage, l.reachUsAt, l.instagram, l.facebook, l.twitter, l.linkedin, r3.name as organiserCollege, r3.profilePic as organiserProfilePic, r3.coverPic as organiserCoverPic from  events as l left join eventorganiser as r3 on l.organiserId = r3.id where l.id = ${req.params.id}`,
+      {
+        type: QueryTypes.SELECT,
+      }
+    );
     if (!event) return res.status(404).json({ message: "Event not found" });
-    return res.status(200).json({ event: event });
+    return res.status(200).json({ event: event[0] });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: error.message });
